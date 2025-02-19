5649df131253147f6adf4bb5690fc7d6a5d05bd4
unknown <eparviai@student.hive.fi>
2025-02-19 19:44:55 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Anzahl Jahre eingeben: ");
+        int jahre = in.nextInt();
+
+        System.out.println("Zinssatz eingeben: ");
+        float zinssatz = in.nextFloat();
+
+        System.out.println("Jetztiger Kontostand: ");
+        float konto = in.nextFloat();
+
+        float zins;
+
+        for (int i = 1; i <= jahre; i++) {
+            zins = zinssatz * konto;
+            konto += zins;
+
+            System.out.printf("Im %d. Jahr gibt es %.2f CHF Zins. Neuer Kontostand: %.2f CHF\n", i,zins,konto);
+
+        }
+
+    }
+}
+
+
+
